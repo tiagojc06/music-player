4dbd5f6569fa7fbfee4c35903b662ef7b67df4eb
tiagojc06 <tiagojcastelo@gmail.com>
2024-12-03 20:13:21 -0300

    Diseñando dropdowns y su lógica de visibilidad, e implementando la funcionalidad al hacer clic en los componentes Button Language y Button Background

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState } from "react";
 import Button from "./Button";
 import titleLogo from "../img/icons/80s-chip-tunes.gif";
 import languageIcon from "../img/icons/language-icon.png";
@@ -12,6 +12,20 @@ import nextIconPressed from "../img/icons/next-icon-pressed.png";
 import "../style-sheets/Header.css";
 
 function Header() {
+
+  const [isLanguageDropdownVisible, setIsLanguageDropdownVisible] = useState(false);
+  const [isBackgroundDropdownVisible, setIsBackgroundDropdownVisible] = useState(false);
+
+  const handleVisibility = type => {
+    if (type === "Language") {
+      setIsLanguageDropdownVisible(!isLanguageDropdownVisible);
+      if (isBackgroundDropdownVisible) setIsBackgroundDropdownVisible(false);
+    } else {
+      setIsBackgroundDropdownVisible(!isBackgroundDropdownVisible);
+      if (isLanguageDropdownVisible) setIsLanguageDropdownVisible(false);
+    }
+  };
+  
   return (
     <header>
       <div className="title-section">
@@ -21,21 +35,45 @@ function Header() {
       <div className="controls-section">
         <div className="control-section">
           <Button
-            handleClick={() => console.log("Language Button Clicked")}
+            handleClick={() => handleVisibility("Language")}
             iconClassName="control-section-icon"
             iconSrc={languageIcon}
             altIconSrc={languageIconPressed}
             iconAlt={"Language Icon"}
           />
+          <div className={`dropdown language-dropdown ${isLanguageDropdownVisible ? "visible" : ""}`.trimEnd()}>
+            <div className="language-dropdown-option">English</div>
+            <div className="language-dropdown-option">Spanish</div>
+          </div>
         </div>
         <div className="control-section">
           <Button 
-            handleClick={() => console.log("Background Button Clicked")}
+            handleClick={() => handleVisibility("Background")}
             iconClassName="control-section-icon"
             iconSrc={backgroundIcon}
             altIconSrc={backgroundIconPressed}
             iconAlt={"Background Icon"}
           />
+          <div className={`dropdown background-dropdown ${isBackgroundDropdownVisible ? "visible" : ""}`.trimEnd()}>
+            <div className="background-dropdown-main-section">
+              <Button 
+                handleClick={() => console.log("Back Button Clicked")}
+                iconClassName="background-dropdown-icon"
+                iconSrc={backIcon}
+                altIconSrc={backIconPressed}
+                iconAlt={"Back Icon"}
+              />
+              <img className="background-dropdown-img" src={require("../img/backgrounds/calme-sous-la-dent.png")} alt="Background selected" />
+              <Button 
+                handleClick={() => console.log("Next Button Clicked")}
+                iconClassName="background-dropdown-icon"
+                iconSrc={nextIcon}
+                altIconSrc={nextIconPressed}
+                iconAlt={"Next Icon"}
+              />
+            </div>
+            <button className="select-background-button">Select</button>
+          </div>
         </div>
       </div>
     </header>
