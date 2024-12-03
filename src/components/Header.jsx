87fc7185da05aedaffd3f0c85ba87983be345e35
tiagojc06 <tiagojcastelo@gmail.com>
2024-12-03 16:30:25 -0300

    Subiendo nuevos iconos, modificando otros e importándolos al componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import titleLogo from "../img/icons/80s-chip-tunes.gif";
 import languageIcon from "../img/icons/language-icon.png";
+import languageIconPressed from "../img/icons/language-icon-pressed.png";
 import backgroundIcon from "../img/icons/background-icon.png";
+import backgroundIconPressed from "../img/icons/background-icon-pressed.png";
+import backIcon from "../img/icons/back-icon.png";
+import backIconPressed from "../img/icons/back-icon-pressed.png";
+import nextIcon from "../img/icons/next-icon.png";
+import nextIconPressed from "../img/icons/next-icon-pressed.png";
 import "../style-sheets/Header.css";
 
 function Header() {
