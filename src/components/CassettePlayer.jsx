f574ff5b4091e4dacd754d49e33079affaf4b824
tiagojc06 <tiagojcastelo@gmail.com>
2024-12-06 18:30:18 -0300

    Diseñando la estructura del componente CassettePlayer

---
+++ src/components/CassettePlayer.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Button from "./Button";
+import playPauseButtonImg from "../img/cassette-player/buttons/play-button.png";
+import playPauseButtonPressedImg from "../img/cassette-player/buttons/play-button-pressed.png";
+import rewindPauseButtonImg from "../img/cassette-player/buttons/rewind-button.png";
+import rewindPauseButtonPressedImg from "../img/cassette-player/buttons/rewind-button-pressed.png";
+import fastForwardButtonImg from "../img/cassette-player/buttons/fast-forward.png";
+import fastForwardButtonPressedImg from "../img/cassette-player/buttons/fast-forward-pressed.png";
+import ejectButtonImg from "../img/cassette-player/buttons/eject-button.png";
+import ejectButtonPressedImg from "../img/cassette-player/buttons/eject-button-pressed.png";
+import "../style-sheets/CassettePlayer.css";
+
+function CassettePlayer() {
+	return (
+		<div className="cassette-player-container">
+			<div className="cassette-player-initial-structure">
+				<div className="cassette-player-song-info">
+					<div className="song-info-title-container">
+						<h2 className="song-info-title">song title</h2>
+					</div>
+					<div className="song-info-timeline-container">
+						<div className="song-info-timeline">
+							<div className="timeline-time-indicator"></div>
+						</div>
+						<span className="song-info-timeline-actual-time">0:00</span>
+					</div>
+				</div>
+				<div className="cassette-player-cassette-cover">
+					<div className="cassette-player-cassette"></div>
+				</div>
+				<div className="cassette-player-controls">
+					<Button
+						handleClick={() => console.log("Play Button Clicked")}
+						imgClassName={"controls-button-img play"}
+						imgSrc={playPauseButtonImg}
+						altImgSrc={playPauseButtonPressedImg}
+						imgAlt={"Play Button"}
+					/>
+					<Button
+						handleClick={() => console.log("Rewind Button Clicked")}
+						imgClassName={"controls-button-img rewind"}
+						imgSrc={rewindPauseButtonImg}
+						altImgSrc={rewindPauseButtonPressedImg}
+						imgAlt={"Rewind Button"}
+					/>
+					<Button
+						handleClick={() => console.log("Fast Forward Button Clicked")}
+						imgClassName={"controls-button-img fast-forward"}
+						imgSrc={fastForwardButtonImg}
+						altImgSrc={fastForwardButtonPressedImg}
+						imgAlt={"Fast Forward Button"}
+					/>
+					<Button
+						handleClick={() => console.log("Eject Button Clicked")}
+						imgClassName={"controls-button-img eject"}
+						imgSrc={ejectButtonImg}
+						altImgSrc={ejectButtonPressedImg}
+						imgAlt={"Eject Button"}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default CassettePlayer;
