3773229e84009bc6bcb8f13e8c1f8f4ae58623fe
tiagojc06 <tiagojcastelo@gmail.com>
2024-12-17 17:02:51 -0300

    Cambiando los props y la clase del componente Button a nombres más entendibles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
 import { React, useState } from "react";
 import "../style-sheets/Button.css";
 
-function Button({ handleClick, iconClassName, iconSrc, altIconSrc, iconAlt }) {
+function Button({ handleClick, imgClassName, imgSrc, altImgSrc, imgAlt }) {
 
 	const [buttonPressed, setButtonPressed] = useState(false);
 
   return (
-		<button className="button" onClick={handleClick} onMouseDown={() => setButtonPressed(true)} onMouseUp={() => setButtonPressed(false)}>
-			<img className={iconClassName} src={buttonPressed ? altIconSrc : iconSrc} alt={iconAlt} />
+		<button className="icon-button" onClick={handleClick} onMouseDown={() => setButtonPressed(true)} onMouseUp={() => setButtonPressed(false)}>
+			<img className={imgClassName} src={buttonPressed ? altImgSrc : imgSrc} alt={imgAlt} />
 		</button>
   );
 }
