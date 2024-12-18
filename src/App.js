3ae78f1c85fa3355e6c2352c359527731b18b2a5
tiagojc06 <tiagojcastelo@gmail.com>
2024-12-17 23:23:40 -0300

    Importando y poniendo el componente CassettePlayer en el componente App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import CassettePlayer from "./components/CassettePlayer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <CassettePlayer />
     </div>
   );
 }
