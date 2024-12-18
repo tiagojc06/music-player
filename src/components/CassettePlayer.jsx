e4fb4c10102a8645fa0eac8ad410058033531b82
tiagojc06 <tiagojcastelo@gmail.com>
2024-12-17 22:52:50 -0300

    Creando y aplicando funcionalidad a ciertos elementos para ajustar el fontSize y lineHeight de su texto para que ocupe toda su altura

--- src/components/CassettePlayer.jsx
+++ src/components/CassettePlayer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useEffect, useRef } from "react";
 import Button from "./Button";
 import playPauseButtonImg from "../img/cassette-player/buttons/play-button.png";
 import playPauseButtonPressedImg from "../img/cassette-player/buttons/play-button-pressed.png";
@@ -10,19 +10,43 @@ import ejectButtonImg from "../img/cassette-player/buttons/eject-button.png";
 import ejectButtonPressedImg from "../img/cassette-player/buttons/eject-button-pressed.png";
 import "../style-sheets/CassettePlayer.css";
 
+function useResponsiveFontSize(ref) {
+  useEffect(() => {
+    const handleResize = () => {
+      if (ref.current) {
+        const height = ref.current.offsetHeight;
+        ref.current.style.fontSize = `${height}px`;
+        ref.current.style.lineHeight = `${height}px`;
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [ref]);
+}
+
 function CassettePlayer() {
+	const songTitleRef = useRef(null);
+	const songActualTime = useRef(null);
+  useResponsiveFontSize(songTitleRef);
+	useResponsiveFontSize(songActualTime);
+	
 	return (
 		<div className="cassette-player-container">
 			<div className="cassette-player-initial-structure">
 				<div className="cassette-player-song-info">
 					<div className="song-info-title-container">
-						<h2 className="song-info-title">song title</h2>
+						<h2 ref={songTitleRef} className="song-info-title">song title</h2>
 					</div>
 					<div className="song-info-timeline-container">
 						<div className="song-info-timeline">
 							<div className="timeline-time-indicator"></div>
 						</div>
-						<span className="song-info-timeline-actual-time">0:00</span>
+						<span ref={songActualTime} className="song-info-timeline-actual-time">0:00</span>
 					</div>
 				</div>
 				<div className="cassette-player-cassette-cover">
